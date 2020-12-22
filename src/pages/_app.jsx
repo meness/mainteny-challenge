5277f4a1bac6b275f39c723854cbaefa08c81b90
Ali Eskandarpour <aesshoferi@gmail.com>
2020-12-22 20:15:41 +0330

    Use and config React Intl for i18n

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,22 @@
 import "@/public/scss/global.scss";
 import Head from "next/head";
+import { IntlProvider } from "react-intl";
+import { useRouter } from "next/router";
+import locales from "intl";
 
 const MaintenyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const { locale, defaultLocale } = router;
+  const messages = locales[locale];
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <IntlProvider locale={locale} defaultLocale={defaultLocale} messages={messages}>
+        <Component {...pageProps} />
+      </IntlProvider>
     </>
   );
 };
