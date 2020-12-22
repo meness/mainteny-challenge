550ada4d2290beb4cb21e566b42b77a6c11c60e8
Ali Eskandarpour <aesshoferi@gmail.com>
2020-12-22 20:10:41 +0330

    Optimize the viewport meta tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,15 @@
 import "@/public/scss/global.scss";
+import Head from "next/head";
 
 const MaintenyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MaintenyApp;
