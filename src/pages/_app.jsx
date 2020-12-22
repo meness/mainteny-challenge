54c3aad9da131e43dabcc5549a67188f72e43463
Ali Eskandarpour <aesshoferi@gmail.com>
2020-12-22 20:18:52 +0330

    Use and config React Toastify

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { IntlProvider } from "react-intl";
 import { useRouter } from "next/router";
 import locales from "intl";
+import { ToastContainer } from "react-toastify";
 
 const MaintenyApp = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -17,6 +18,17 @@ const MaintenyApp = ({ Component, pageProps }) => {
       <IntlProvider locale={locale} defaultLocale={defaultLocale} messages={messages}>
         <Component {...pageProps} />
       </IntlProvider>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 };
